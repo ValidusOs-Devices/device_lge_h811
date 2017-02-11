24832bed0117970e520e751c78a10ca3d3bbb5dc
Adam <ovadia.adam@gmail.com>
2017-02-10 20:48:40 -0500

    Update and rename tesla.mk to validus.mk

--- validus.mk
+++ validus.mk
@@ -1,18 +1,19 @@
 $(call inherit-product, device/lge/h811/full_h811.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
-$(call inherit-product, vendor/tesla/config/caf_fw.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/caf_fw.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_NAME := tesla_h811
+PRODUCT_NAME := validus_h811
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Adam Ovadia (oadam11)" \
     PRODUCT_DEVICE="g4" \
     PRODUCT_NAME="g4__tmo_us" \
     BUILD_FINGERPRINT="lge/p1__tmo_us/p1:6.0/MRA58K/152940055675e:user/release-keys" \
